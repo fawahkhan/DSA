c7127ab571a38cba9d10646c3c2d859866643ad6
fawahkhan <khanmohdfawah21@gmail.com>
2026-04-20 04:03:06 +0530

    Lecture 58 done

---
+++ LinkedLists/58-ReverseLL/code.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+class Solution {
+public:
+    struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode() : val(0), next(nullptr) {}
+     ListNode(int x) : val(x), next(nullptr) {}
+     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ };
+    ListNode* reverseList(ListNode* head) {
+       ListNode* prev = NULL;
+       ListNode* curr = head;
+       ListNode* next = NULL;
+
+       while( curr != NULL){
+        next = curr->next ;  // sabse pehle hame next ko preserve krna hai taaki saved rhe
+        curr -> next = prev ; // ab connection break krdiya the next of current is assigned to prev 
+
+        prev = curr ; // ab previous ko current bana diya
+        curr = next ; //aur curr ko next bana diya and run the loop again
+       }
+       return prev ;
+    }
+
+};
